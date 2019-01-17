df2bc4190410cd182adffec91377dc99e3658803
Your Name <github email address>
2019-01-17 13:20:36 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,66 @@
+-- 20 users
+INSERT INTO users VALUES (1, "Sivan", 24);
+INSERT INTO users VALUES (2, "Sarah", 23);
+INSERT INTO users VALUES (3, "Natalie", 34);
+INSERT INTO users VALUES (4, "Sam", 28);
+INSERT INTO users VALUES (5, "Arielle", 24);
+INSERT INTO users VALUES (6, "Ari", 30);
+INSERT INTO users VALUES (7, "Mark", 26);
+INSERT INTO users VALUES (8, "Kate", 26);
+INSERT INTO users VALUES (9, "Talia", 24);
+INSERT INTO users VALUES (10, "Zack", 29);
+INSERT INTO users VALUES (11, "Trey", 22);
+INSERT INTO users VALUES (12, "Dan", 29);
+INSERT INTO users VALUES (13, "Serena", 31);
+INSERT INTO users VALUES (14, "Blair", 20);
+INSERT INTO users VALUES (15, "Nate", 30);
+INSERT INTO users VALUES (16, "Georgina", 22);
+INSERT INTO users VALUES (17, "Chuck", 24);
+INSERT INTO users VALUES (18, "Rufus", 44);
+INSERT INTO users VALUES (19, "Ann", 40);
+INSERT INTO users VALUES (20, "Eric", 17);
+
+-- 10 projects
+INSERT INTO projects VALUES (1, "project 1", "presentation", 2400, "01/01/2019", "04/19/2019");
+INSERT INTO projects VALUES (2, "project 2", "new app", 24000, "02/17/2019", "02/17/2020");
+INSERT INTO projects VALUES (3, "project 3", "pitch", 300, "01/01/2019", "01/01/2020");
+INSERT INTO projects VALUES (4, "project 4", "presentation", 700000, "01/01/2019", "04/19/2019");
+INSERT INTO projects VALUES (5, "project 5", "new app", 800, "08/01/2019", "12/31/2019");
+INSERT INTO projects VALUES (6, "project 6", "pitch", 140000, "09/01/2019", "04/19/2020");
+INSERT INTO projects VALUES (7, "project 7", "presentation", 200, "02/12/2019", "10/19/2019");
+INSERT INTO projects VALUES (8, "project 8", "presentation", 2400, "12/12/2019", "12/12/2020");
+INSERT INTO projects VALUES (9, "project 9", "new app", 8900, "01/01/2019", "04/19/2019");
+INSERT INTO projects VALUES (10, "project 10", "pitch", 1000000000, "10/01/2019", "04/19/2020");
+
+
+--30 pledges
+INSERT INTO pledges VALUES (1, 300, "user 1", "project 1");
+INSERT INTO pledges VALUES (2, 400, "user 2", "project 2");
+INSERT INTO pledges VALUES (3, 90000, "user 3", "project 3");
+INSERT INTO pledges VALUES (4, 30, "user 4", "project 4");
+INSERT INTO pledges VALUES (5, 4000, "user 5", "project 5");
+INSERT INTO pledges VALUES (6, 100, "user 6", "project 6");
+INSERT INTO pledges VALUES (7, 20000, "user 7", "project 7");
+INSERT INTO pledges VALUES (8, 3000000, "user 8", "project 8");
+INSERT INTO pledges VALUES (9, 30, "user 9", "project 9");
+INSERT INTO pledges VALUES (10, 10, "user 10", "project 10");
+INSERT INTO pledges VALUES (11, 300, "user 11", "project 11");
+INSERT INTO pledges VALUES (12, 40000, "user 12", "project 12");
+INSERT INTO pledges VALUES (13, 30, "user 13", "project 13");
+INSERT INTO pledges VALUES (14, 800, "user 14", "project 14");
+INSERT INTO pledges VALUES (15, 900, "user 15", "project 15");
+INSERT INTO pledges VALUES (16, 1000, "user 16", "project 16");
+INSERT INTO pledges VALUES (17, 30, "user 17", "project 17");
+INSERT INTO pledges VALUES (18, 100, "user 18", "project 18");
+INSERT INTO pledges VALUES (19, 3000, "user 19", "project 19");
+INSERT INTO pledges VALUES (20, 390, "user 20", "project 20");
+INSERT INTO pledges VALUES (21, 30800, "user 21", "project 21");
+INSERT INTO pledges VALUES (22, 30700, "user 22", "project 22");
+INSERT INTO pledges VALUES (23, 700, "user 23", "project 23");
+INSERT INTO pledges VALUES (24, 3900, "user 24", "project 24");
+INSERT INTO pledges VALUES (25, 1000, "user 25", "project 25");
+INSERT INTO pledges VALUES (26, 30, "user 26", "project 26");
+INSERT INTO pledges VALUES (27, 100, "user 27", "project 27");
+INSERT INTO pledges VALUES (28, 400, "user 28", "project 28");
+INSERT INTO pledges VALUES (29, 700, "user 29", "project 29");
+INSERT INTO pledges VALUES (30, 300000, "user 30", "project 30");
