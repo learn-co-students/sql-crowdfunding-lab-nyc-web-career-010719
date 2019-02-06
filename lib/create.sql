9333feda43cf6575fc5ece837dfc513386cfe280
github username <github email address>
2019-02-06 17:52:03 -0500

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,58 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY,
-title TEXT, category TEXT, funding_goal INT, start_date TEXT, end_date TEXT);
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INT,
+  start_date TEXT,
+  end_date TEXT
+);
 
-CREATE TABLE users (id INTEGER PRIMARY KEY,
-name TEXT, age INTEGER );
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INT
+);
 
-CREATE TABLE pledges (id INTEGER PRIMARY KEY,
-amount INTEGER, user_id INTEGER, project_id INTEGER)
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INT,
+  user_id INT,
+  project_id INT
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- CREATE TABLE projects (id INTEGER PRIMARY KEY,
+-- title TEXT, category TEXT, funding_goal INT, start_date TEXT, end_date TEXT);
+--
+-- CREATE TABLE users (id INTEGER PRIMARY KEY,
+-- name TEXT, age INTEGER );
+--
+-- CREATE TABLE pledges (id INTEGER PRIMARY KEY,
+-- amount INTEGER, user_id INTEGER, project_id INTEGER)
